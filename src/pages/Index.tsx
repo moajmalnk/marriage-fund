96d0bc127795e69cf06f86b286ce458392e67f3f
moajmalnk <moajmalnk@gmail.com>
2025-10-18 23:04:02 +0530

    updated by nk

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -306,7 +306,7 @@ const Index = () => {
             </div>
           </div>
           <div className="border-t border-slate-800 mt-12 pt-8 text-center text-slate-400">
-            <p>&copy; 2024 CBMS Marriage Fund. All rights reserved.</p>
+            <p>&copy; 2025 CBMS Marriage Fund. All rights reserved.</p>
           </div>
         </div>
       </footer>
